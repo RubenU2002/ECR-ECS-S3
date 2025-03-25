b9a682ef407228877877eff8b77e75ef91a0fea8
RubenSantiagoUrrego <rurrego@innovapps.net>
2025-03-24 21:07:34 -0500

    feat: 🎸 new endpoint

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send(`Environment variable value: ${process.env.MSJ}`);
+    res.send(`Se hizo una segunda deploy: ${process.env.MSJ}`);
+});
+app.get('/daniel', (req, res) => {
+    res.send(`Ola mamayema`);
 });
 
 app.listen(port, () => {
